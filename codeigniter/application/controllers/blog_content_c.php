a12cb541afaf5084b3bce980a8ae1410a040adfe
tangxueer <378711563@qq.com>
2016-02-17 21:13:36 +0800

    增加基本的注册登录功能

--- codeigniter/application/controllers/blog_content_c.php
+++ codeigniter/application/controllers/blog_content_c.php
@@ -13,23 +13,25 @@ class Blog_content_c extends CI_Controller
 		$this->load->model('blog_m');
 
 		$arr=array('title'=>$_POST['title'],'content'=>$_POST['content'],'date'=>date("Y-m-d"));
-		$insert=$this->blog_m->insert($arr);
-		$arow=$this->blog_m->affected_rows();//影响条数
-
-		if($arow='1')
-		{	//跳转至首页
-			echo '
-            <script language="javascript"> 
-                alert("发布成功!"); 
-                window.location.href="http://localhost/codeigniter/index.php/blog_content_c/index"; 
-            </script> ';     
-		}else
-		{	//跳转至发布博客页面
-			echo '
-			<script language="javascript">
-				alert("发布失败!请重试");
-				window.location.href="http://localhost/codeigniter/index.php/blog_content_c/content_index";
-			</script> ';
+		if($_POST['submit'])
+		{
+			$insert=$this->blog_m->insert($arr);
+			$arow=$this->blog_m->affected_rows();//影响条数
+			if($arow='1')
+			{	//跳转至首页
+				echo '
+				<script language="javascript"> 
+					alert("发布成功!"); 
+					window.location.href="http://localhost/codeigniter/index.php/blog_content_c/index"; 
+				</script> ';     
+			}else
+			{	//跳转至发布博客页面
+				echo '
+				<script language="javascript">
+					alert("发布失败!请重试");
+					window.location.href="http://localhost/codeigniter/index.php/blog_content_c/content_index";
+				</script> ';
+			}
 		}
 				
 	}
@@ -62,16 +64,30 @@ class Blog_content_c extends CI_Controller
 		$this->load->view('blog_index',$data);			
 	}
 	
-	
+
 	/*博客具体内容页面*/
 	function view($id)
 	{
 		$this->load->model('blog_m');
-		$data['single']=$this->blog_m->select($id);
-		$this->load->view('blog_view',$data);		
+		$data['single']=$this->blog_m->select($id);		
 		
-		/*删除处理*/
-		if($_POST['submit'])
+		/*登录后才允许评论功能,验证用户是否已经登录*/
+		$this->load->library('session');
+		$data['session']=$this->session->userdata('uid');
+
+		
+		/*把用户评论存入后台数据库(没做完)*/
+		
+		/*$this->load->model('user_m');
+		$arr=array('');
+		$this->user_m->insert($arr);	
+		*/
+		
+		$this->load->view('blog_view',$data);		
+
+		
+		/*删除博客处理*/
+		if($_POST['sub_del'])
 		{
 			$this->load->model('blog_m');
 			echo '
@@ -84,7 +100,104 @@ class Blog_content_c extends CI_Controller
 				}
 			</script> ';
 		}
-		
+
 	}
+	
+	
+	/*用户注册*/
+	function register()
+	{
+		$this->load->view('blog_register');
+		
+		$this->load->model('user_m');		
+		$arr=array('uname'=>$_POST['uname'],'upass'=>$_POST['upass']);
+		if($_POST['submit'])
+		{
+			$this->user_m->user_insert($arr);
+			$arow=$this->user_m->affected_rows();//影响条数
+			if($arow='1')
+			{	//跳转至登录页面
+				echo '
+				<script language="javascript"> 
+					alert("注册成功!"); 
+					window.location.href="http://localhost/codeigniter/index.php/blog_content_c/login"; 
+				</script> ';     
+			}else
+			{	//跳转至注册页面
+				echo '
+				<script language="javascript">
+					alert("注册失败!请重试");
+					window.location.href="http://localhost/codeigniter/index.php/blog_content_c/register";
+				</script> ';
+			}		
+		}
+	}
+	
+	/*用户登录前端页面*/
+	function login()
+	{
+		$this->load->view('blog_login');		
+	}	
+	/*用户登录后台数据库比对*/
+	function checklogin()
+	{
+		$this->load->model('user_m');
+		$user=$this->user_m->user_select($_POST['uname']);
+		if($_POST['submit'])
+		{
+			if($user)
+			{
+				if($user[0]->upass==$_POST['upass'])
+				{
+					if($_POST['auto'])
+					{	//开启session			
+						$this->load->library('session');
+						$arr=array('uid'=>$user[0]->uid);
+						$this->session->set_userdata($arr);
+						$this->session->userdata('uid');
+					}
+					echo '
+						<script language="javascript">
+							alert("登录成功!");
+							window.location.href="http://localhost/codeigniter/index.php/blog_content_c/index";
+						</script> ';
+					
+				}else
+				{
+					echo '
+						<script language="javascript">
+							alert("登录失败！请重试");
+							window.location.href="http://localhost/codeigniter/index.php/blog_content_c/login";
+						</script> ';
+				}
+			}else
+			{
+				echo '
+						<script language="javascript">
+							alert("该用户名不存在，请注册");
+							window.location.href="http://localhost/codeigniter/index.php/blog_content_c/login";
+						</script> ';					
+			}	
+		}		
+
+	}
+	
+
+	/*用户退出登录*/
+	function loginout()
+	{
+		$this->load->library('session');
+		$this->session->unset_userdata('uid');
+		echo'
+			<script language="javascript">
+				var out=confirm("您确定要退出吗？");
+				if(out==true)
+				{
+					
+				}
+				window.location.href="http://localhost/codeigniter/index.php/blog_content_c/index";
+			</script> ';		
+	}
+	
 }
 ?>
--- codeigniter/application/views/blog_view.php
+++ codeigniter/application/views/blog_view.php
@@ -4,26 +4,45 @@
 <title>博客具体内容页面</title>
 </head>
 <body>
+<a href="/codeigniter/index.php/blog_content_c/index" >返回首页</a>
 
 <h2>
 <hr>
-	<?php echo $single[0]['title'];?>
+	<?php echo $single['title'];?>
 
 </h2>
 
 <p>
-	<?php echo $single[0]['date'];?>
+	<?php echo $single['date'];?>
+	<?php echo $single['hit'];?>
 	
-<form action="<?php echo "/codeigniter/index.php/blog_content_c/view/".$single[0]['cid'];?>" method="post">
-	<input type="submit" name="submit" value="删除"/>
+<form action="<?php echo "/codeigniter/index.php/blog_content_c/view/".$single['cid'];?>" method="post">
+	<input type="submit" name="sub_del" value="删除"/>
 </form>
 
 </p>
 
 <div>
 <hr>
-	<?php echo $single[0]['content'];?>
+	<?php echo $single['content'];?>
+<hr>
 </div>
+
+<!--显示评论 -->
+
+
+
+
+<!--用户发表新评论-->
+<div>
+发表评论：<br>
+
+<?php if(!$session){?>请先<a href="/codeigniter/index.php/blog_content_c/login" >登录</a>后再发表评论
+<?php }else{?><textarea rows="5" cols="100" name="comments" ></textarea>
+<?php }?>
+
+</div>
+
 
 </body>
 </html>
